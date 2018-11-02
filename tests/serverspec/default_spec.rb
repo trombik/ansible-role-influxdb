e06d045a345eae6aca785511f5909982111bb8d4
Tomoyuki Sakurai <y@trombik.org>
2018-11-02 17:58:32 +0900

    feature: initial implementation

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -3,33 +3,43 @@ require "serverspec"
 
 package = "influxdb"
 service = "influxdb"
-config  = "/etc/influxdb/influxdb.conf"
+config_dir = "/etc/influxdb"
+config_name = "influxdb.conf"
 user    = "influxdb"
 group   = "influxdb"
-ports   = [PORTS]
-log_dir = "/var/log/influxdb"
+ports   = [8088, 8086]
 db_dir  = "/var/lib/influxdb"
+default_user = "root"
+default_group = "root"
 
 case os[:family]
 when "freebsd"
-  config = "/usr/local/etc/influxdb.conf"
+  service = "influxd"
+  user = "influxd"
+  group = "influxd"
+  config_dir = "/usr/local/etc"
+  config_name = "influxd.conf"
   db_dir = "/var/db/influxdb"
+  default_group = "wheel"
+when "openbsd"
+  user = "_influx"
+  group = "_influx"
+  db_dir = "/var/influxdb"
 end
+config = "#{config_dir}/#{config_name}"
 
 describe package(package) do
   it { should be_installed }
 end
 
 describe file(config) do
-  it { should be_file }
-  its(:content) { should match Regexp.escape("influxdb") }
-end
-
-describe file(log_dir) do
   it { should exist }
-  it { should be_mode 755 }
-  it { should be_owned_by user }
-  it { should be_grouped_into group }
+  it { should be_file }
+  it { should be_mode os[:family] == "openbsd" ? 640 : 644 }
+  it { should be_owned_by os[:family] == "openbsd" ? user : default_user }
+  it { should be_grouped_into os[:family] == "openbsd" ? group : default_group }
+  its(:content) { should match(/^# Managed by ansible$/) }
+  its(:content) { should match(/^reporting-disabled = true$/) }
 end
 
 describe file(db_dir) do
@@ -39,10 +49,29 @@ describe file(db_dir) do
   it { should be_grouped_into group }
 end
 
+%w[data meta wal].each do |d|
+  describe file "#{db_dir}/#{d}" do
+    it { should exist }
+    it { should be_directory }
+    it { should be_mode d == "wal" ? 700 : 755 }
+    it { should be_owned_by user }
+    it { should be_grouped_into group }
+  end
+end
+
 case os[:family]
 when "freebsd"
-  describe file("/etc/rc.conf.d/influxdb") do
+  describe file("/etc/rc.conf.d/#{service}") do
+    it { should exist }
     it { should be_file }
+    it { should be_mode 644 }
+    it { should be_owned_by default_user }
+    it { should be_grouped_into default_group }
+    its(:content) { should match(/^# Managed by ansible$/) }
+    its(:content) { should match(/^influxd_conf="#{config}"$/) }
+    its(:content) { should match(/^influxd_user="#{user}"$/) }
+    its(:content) { should match(/^influxd_group="#{group}"$/) }
+    its(:content) { should match(/^influxd_flags=""$/) }
   end
 end
 
@@ -55,4 +84,10 @@ ports.each do |p|
   describe port(p) do
     it { should be_listening }
   end
+end
+
+describe command "influx -execute 'show databases'" do
+  its(:exit_status) { should eq 0 }
+  its(:stderr) { should eq "" }
+  its(:stdout) { should match(/^name: databases\nname\n\----\n_internal$/) }
 end
